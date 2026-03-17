4e2bc3534604698be968be5ee9b297bfe7da3b55
3wguap <rnbrell@yandex.by>
2026-03-17 20:52:19 +0400

    add OrderItem(model)

---
+++ src/main/java/ru/yandex/practicum/mymarket/orderItem/model/OrderItem.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.mymarket.orderItem.model;
+
+import jakarta.persistence.*;
+import ru.yandex.practicum.mymarket.item.model.Item;
+import ru.yandex.practicum.mymarket.order.model.Order;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer count;
+
+    @Column(nullable = false)
+    private Long price;
+
+    public Long getTotalPrice() {
+        return this.price * this.count;
+    }
+}
