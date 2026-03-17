e19287ace3f8e782ffc9363213a3a0241fa5956c
3wguap <rnbrell@yandex.by>
2026-03-17 20:52:50 +0400

    add Order(dto)

---
+++ src/main/java/ru/yandex/practicum/mymarket/order/dto/OrderDto.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.mymarket.order.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import ru.yandex.practicum.mymarket.orderItem.dto.OrderItemDto;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderDto {
+    private List<OrderItemDto> items;
+    private long totalSum;
+}
