122ecbbd40c84fe31e3038f2ebb57af8d8519b6f
3wguap <rnbrell@yandex.by>
2026-03-18 19:43:15 +0400

    add Item(repo)

---
+++ src/main/java/ru/yandex/practicum/mymarket/item/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.mymarket.item.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.yandex.practicum.mymarket.item.model.Item;
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
+    @Query("SELECT i FROM Item i WHERE LOWER(i.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(i.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<Item> searchByTitleOrDescription(@Param("keyword") String keyword, Pageable pageable);
+}
